a5c92f6bfdaa76e40812a7379780b4535c8c7cb3
yyh-gl <yhonda.95.gl@gmail.com>
2020-06-07 02:46:02 +0900

    Fix github test

--- internal/repository/github/github_test.go
+++ internal/repository/github/github_test.go
@@ -1,20 +1,22 @@
 package github_test
 
 import (
+	"context"
 	"net/http"
 	"testing"
 
 	"github.com/dnaeon/go-vcr/recorder"
 	"github.com/stretchr/testify/assert"
-	"github.com/yyh-gl/gocr/internal/github"
+	"github.com/yyh-gl/gocr/internal/repository"
+	"github.com/yyh-gl/gocr/internal/repository/github"
 )
 
-func Test_GeneralClient_FetchPullRequestDetails(t *testing.T) {
+func Test_FetchPullRequestDetails_NotForEnterprise(t *testing.T) {
 	testCases := []struct {
 		name     string
 		owner    string
 		repoName string
-		want     *github.PullRequests
+		want     repository.CodeReviewRequests
 	}{
 		{
 			name:     "Success",
@@ -22,28 +24,34 @@ func Test_GeneralClient_FetchPullRequestDetails(t *testing.T) {
 			repoName: "gocr",
 			want: &github.PullRequests{
 				github.PullRequest{
-					Title:          "test",
-					HTMLURL:        "https://github.com/yyh-gl/gocr/pull/45",
-					Reviewers:      []github.Reviewer{{Login: "yyh-gl-robo"}},
+					Title:          "Update go version",
+					LinkURL:        "https://github.com/yyh-gl/gocr/pull/2",
+					Reviewers:      []github.Reviewer{},
 					MergeableState: "clean",
 				},
 			},
 		},
 	}
 
-	r, _ := recorder.New("../../test/cassettes/GeneralClient_FetchPullRequestDetails")
+	r, _ := recorder.New("../../../test/cassettes/FetchPullRequestDetails_NotForEnterprise")
 	defer func() { _ = r.Stop() }()
 	hc := http.DefaultClient
 	hc.Transport = r
 
-	// You set access token when recording response.
-	// And you must remove access token when commit.
-	at := ""
-	c := github.NewGeneralClient(hc, at)
+	repo := map[interface{}]interface{}{
+		"name":  "gocr",
+		"owner": "yyh-gl",
+		// You set access token when recording response.
+		// And you must change access token to "test-token" when commit.
+		// Please also change the access token on the cassette.
+		"access_token": "test-token",
+		"sender":       "test",
+	}
+	c := github.NewClient(hc, repo)
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got, err := c.FetchPullRequestDetails(tc.owner, tc.repoName)
+			got, err := c.FetchCodeReviewRequests(context.Background())
 			assert.Nil(t, err)
 			assert.Equal(t, tc.want, got)
 		})
