9b7887daf418c28cbc2efeed38922187dcf6c4f3
yyh-gl <yhonda.95.gl@gmail.com>
2020-06-07 02:11:17 +0900

    Change package structure

---
+++ internal/loader.go
@@ -0,0 +1,42 @@
+package loader
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type (
+	ConfigTemplate struct {
+		RepositorySet RepositorySet `yaml:"repositories"`
+		SenderSet     SenderSet     `yaml:"senders"`
+	}
+
+	RepositorySet map[string]map[string]interface{}
+
+	SenderSet map[string]map[string]interface{}
+)
+
+func LoadConfigFile(configPath string) *ConfigTemplate {
+	cp := strings.Replace(configPath, "yaml", "yml", 1)
+	f, err := os.Open(cp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var ct ConfigTemplate
+	err = yaml.Unmarshal(b, &ct)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return &ct
+}
