1ca01a92577fa71df037ddba70166c8c9974f301
yyh-gl <yhonda.95.gl@gmail.com>
2020-06-07 02:16:09 +0900

    Fix cli logic

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/yyh-gl/gocr/internal/github"
-	"github.com/yyh-gl/gocr/internal/slack"
-	"github.com/yyh-gl/gocr/internal/yaml"
+	"github.com/yyh-gl/gocr/internal"
 )
 
 var (
@@ -20,28 +18,10 @@ var (
 		Short:   "GoCR is planner for a notification of code review request.",
 		Long:    "GoCR provides easy way for notifying request of code review.\nYou will soon be able to start a notification of code review request.",
 		Run: func(cmd *cobra.Command, args []string) {
-			ct := yaml.LoadConfigFile(configPath)
-
-			for _, r := range ct.Repositories {
-				var c *github.Client
-				if r.IsEnterprise {
-					c = github.NewEnterpriseClient(http.DefaultClient, r.EnterpriseHost, r.AccessToken)
-				} else {
-					c = github.NewGeneralClient(http.DefaultClient, r.AccessToken)
-				}
-
-				prs, err := c.FetchPullRequestDetails(r.Owner, r.Name)
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				if len(*prs) > 0 {
-					ss := ct.Slacks[r.SlackID]
-					sc := slack.NewClient(ss.WebHook, ss.Channel, ss.Username, ss.IconEmoji)
-
-					msg := slack.CreateMessage(prs.ConvertToSlackDTOs(), ss.UserMap)
-					sc.Send(r.Name, msg)
-				}
+			m := internal.NewManger(configPath)
+			if err := m.Do(context.Background()); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
 			}
 		},
 	}
